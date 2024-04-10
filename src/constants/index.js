91625014261dcee159bbe454685d29a34f5b1c72
Krish Behera <krishbehera10@gmail.com>
2024-04-10 08:41:25 -0400

    added actual math qs

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,58 +1,148 @@
 export const questions = [
+    // 2nd grade math questions
     {
-      "question": "What is 1+1?",
-      "answer1": "7",
-      "answer2": "2",
+      "question": "What is 10 + 5?",
+      "answer1": "15",
+      "answer2": "12",
+      "answer3": "8",
+      "answer4": "20"
+    },
+    {
+      "question": "How many sides does a square have?",
+      "answer1": "3",
+      "answer2": "4",
+      "answer3": "5",
+      "answer4": "6"
+    },
+    {
+      "question": "What is 6 x 3?",
+      "answer1": "9",
+      "answer2": "15",
+      "answer3": "12",
+      "answer4": "18"
+    },
+    {
+      "question": "How many months are in a year?",
+      "answer1": "10",
+      "answer2": "12",
       "answer3": "6",
-      "answer4": "22",
-      "answer5": "5",
-      "answer6": "11",
+      "answer4": "8"
+    },
+    // 3rd grade math questions
+    {
+      "question": "What is 25 - 13?",
+      "answer1": "16",
+      "answer2": "12",
+      "answer3": "15",
+      "answer4": "8"
     },
     {
-        "question": "What is 7 x 8?",
-        "answer1": "7",
-        "answer2": "2",
-        "answer3": "56",
-        "answer4": "32",
-        "answer5": "1",
-        "answer6": "91",
+      "question": "How many minutes are in an hour?",
+      "answer1": "60",
+      "answer2": "30",
+      "answer3": "45",
+      "answer4": "90"
     },
     {
-        "question": "What is 6 / 2?",
-        "answer1": "7",
-        "answer2": "3",
-        "answer3": "1",
-        "answer4": "12",
-        "answer5": "99",
-        "answer6": "11",
+      "question": "What is 7 x 7?",
+      "answer1": "14",
+      "answer2": "36",
+      "answer3": "56",
+      "answer4": "49"
     },
     {
-        "question": "What is 72/30?",
-        "answer1": "2.4",
-        "answer2": "2000",
-        "answer3": "6222",
-        "answer4": "2220",
-        "answer5": "2160",
-        "answer6": "1211",
-
+      "question": "How many continents are there?",
+      "answer1": "2",
+      "answer2": "5",
+      "answer3": "7",
+      "answer4": "4"
+    },
+    // 4th grade math questions
+    {
+      "question": "What is 345 + 189?",
+      "answer1": "534",
+      "answer2": "434",
+      "answer3": "524",
+      "answer4": "624"
     },
     {
-        "question": "What is the square root of 144?",
-        "answer1": "1441440",
-        "answer2": "144",
-        "answer3": "12",
-        "answer4": "6",
-        "answer5": "2",
-        "answer6": "1",
+      "question": "How many degrees are in a right angle?",
+      "answer1": "45",
+      "answer2": "180",
+      "answer3": "90",
+      "answer4": "360"
     },
     {
-        "question": "What is 99 squared?",
-        "answer1": "1",
-        "answer2": "9801",
-        "answer3": "99",
-        "answer4": "33",
-        "answer5": "2",
-        "answer6": "9.9",
+      "question": "What is the perimeter of a square with side length 5 units?",
+      "answer1": "20",
+      "answer2": "25",
+      "answer3": "15",
+      "answer4": "10"
     },
-  ];
+    {
+      "question": "If a box contains 24 apples and you take out 6, how many are left?",
+      "answer1": "30",
+      "answer2": "18",
+      "answer3": "20",
+      "answer4": "22"
+    },
+    // 5th grade math questions
+    {
+      "question": "What is the result of 3 x (4 + 5)?",
+      "answer1": "12",
+      "answer2": "21",
+      "answer3": "18",
+      "answer4": "27"
+    },
+    {
+      "question": "Simplify: 2/3 + 1/4",
+      "answer1": "11/12",
+      "answer2": "5/7",
+      "answer3": "3/4",
+      "answer4": "7/8"
+    },
+    {
+      "question": "What is the square root of 144?",
+      "answer1": "12",
+      "answer2": "14",
+      "answer3": "16",
+      "answer4": "18"
+    },
+    {
+      "question": "If a car travels at 60 mph, how far will it go in 3 hours?",
+      "answer1": "190 miles",
+      "answer2": "160 miles",
+      "answer3": "180 miles",
+      "answer4": "220 miles"
+    },
+    // 6th grade math questions
+    {
+      "question": "What is the result of 5^2?",
+      "answer1": "35",
+      "answer2": "20",
+      "answer3": "30",
+      "answer4": "25"
+    },
+    {
+      "question": "Solve for x in the equation 2x + 3 = 15",
+      "answer1": "7",
+      "answer2": "8",
+      "answer3": "6",
+      "answer4": "10"
+    },
+    {
+        "question": "What is the area of a triangle with base 6 units and height 8 units?",
+        "answer1": "24 square units",
+        "answer2": "30 square units",
+        "answer3": "36 square units",
+        "answer4": "40 square units"
+    },
+    {
+        "question": "If a recipe calls for 2/3 cup of flour and you want to make 3 batches, how much flour do you need?",
+        "answer1": "2 cups",
+        "answer2": "1 cup",
+        "answer3": "1.5 cups",
+        "answer4": "3 cups"
+    },
+  ]
 
