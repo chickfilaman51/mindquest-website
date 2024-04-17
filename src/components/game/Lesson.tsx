37805af455f00b9491d94803adfbd711ba02d2e9
Krish Behera <krishbehera10@gmail.com>
2024-04-16 23:11:49 -0400

    formatted video

--- src/components/game/Lesson.tsx
+++ src/components/game/Lesson.tsx
@@ -2,22 +2,28 @@ import React from 'react';
 import { lessonIndex } from './Dashboard';
 import { gradeLessons } from '~/constants';
 import { grade } from '../placement/PlacementResults';
+// Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 const Lesson = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold mb-4">{gradeLessons[grade][lessonIndex]}</h1>
-      <div className="w-full lg:w-2/3 h-96">
-        <iframe
-          width="100%"
-          height="100%"
-          src="https://www.youtube.com/embed/z_Xjxqk8E4g"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
+        <h1 className="text-3xl font-bold mb-4">{gradeLessons[grade][lessonIndex]}</h1>
+        <div className="w-full lg:w-1/2 xl:w-1/3 h-auto aspect-video">
+            <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/z_Xjxqk8E4g"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+            ></iframe>
+        </div>
+        {/* Link to Questions module */}
+        <Link to="/questions" className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
+            Go to Questions
+        </Link>
     </div>
   );
 };
